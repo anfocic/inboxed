aee2972750dca34369174bdfdccca8e60117aec5
fole <a_fole@protonmail.com>
2025-07-04 00:21:32 +0100

    Stop tracking dist folder

--- dist/utils/logger.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logger = void 0;
-exports.logger = {
-    log: (...args) => console.log(...args),
-    error: (...args) => console.error(...args),
-};
